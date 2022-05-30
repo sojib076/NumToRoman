ccde28ca509af3cebea7b63251a8027f9a961b35
sojib076 <sojibds332@gmail.com>
2022-05-30 17:36:43 +0600

    push

--- js.js
+++ js.js
@@ -1,32 +1,4 @@
 
-function numToRoman(num) {
-    var romanToNum = {
-        M: 1000,
-        CM: 900,
-        d: 500,
-        CD: 400,
-        C: 100,
-        XC: 90,
-        L: 50,
-        XL: 40,
-        X: 10,
-        IX: 9,
-        V: 5,
-        IV: 4,
-        I: 1,
-    };
-    var roman = "";
-
-    for (var x in romanToNum) {
-        while (num >= romanToNum[x]) {
-            roman += x;
-            num -= romanToNum[x];
-
-        }
-
-    }
-
-}
 
 function sojib() {
     var age2 = document.getElementById("age").value;
